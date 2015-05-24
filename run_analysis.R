8c93ca274c01fd0b6c328bcb0f27a57ecdda6cee
Amrut Rajkarne <amathraj@gmail.com>
2015-05-24 19:13:44 +0530

    run_analysis.R file added

--- run_analysis.R
+++ run_analysis.R
@@ -1,17 +1,4 @@
-## Create one R script called run_analysis.R that does the following:
-## 1. Merges the training and the test sets to create one data set.
-## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
-## 3. Uses descriptive activity names to name the activities in the data set
-## 4. Appropriately labels the data set with descriptive activity names.
-## 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-
-if (!require("data.table")) {
-  install.packages("data.table")
-}
-
-if (!require("reshape2")) {
-  install.packages("reshape2")
-}
+# Ensure that the packages data.table and reshape2 are installed otherwise install them first
 
 require("data.table")
 require("reshape2")
